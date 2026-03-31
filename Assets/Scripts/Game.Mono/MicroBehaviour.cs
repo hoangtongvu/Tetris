e8849c32c033ee6cb9ae9d917d59614f4a7d5aea
hoangtongvu <hoangtongvu@gmail.com>
2026-03-31 13:43:00 +0700

    Add MicroBehaviour + Executor

---
+++ Assets/Scripts/Game.Mono/MicroBehaviour.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Game.Mono;
+
+[Serializable]
+public abstract class MicroBehaviour
+{
+    public virtual void Awake() { }
+
+    public virtual void Start() { }
+
+    public virtual void OnDestroy() { }
+
+    public virtual void FixedUpdate() { }
+
+    public virtual void Update() { }
+
+    public virtual void OnEnable() { }
+
+    public virtual void OnDisable() { }
+}
---
+++ Assets/Scripts/Game.Mono/MicroBehavioursExecutor.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Mono
+{
+    public class MicroBehavioursExecutor : MonoBehaviour
+    {
+        [SerializeReference, SubclassSelector]
+        private List<MicroBehaviour> microBehaviours;
+
+        private void Awake()
+        {
+            foreach (var microBehaviour in this.microBehaviours)
+            {
+                microBehaviour.Awake();
+            }
+        }
+
+        private void Start()
+        {
+            foreach (var microBehaviour in this.microBehaviours)
+            {
+                microBehaviour.Start();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            foreach (var microBehaviour in this.microBehaviours)
+            {
+                microBehaviour.FixedUpdate();
+            }
+        }
+
+        private void Update()
+        {
+            foreach (var microBehaviour in this.microBehaviours)
+            {
+                microBehaviour.Update();
+            }
+        }
+
+        private void OnEnable()
+        {
+            foreach (var microBehaviour in this.microBehaviours)
+            {
+                microBehaviour.OnEnable();
+            }
+        }
+
+        private void OnDisable()
+        {
+            foreach (var microBehaviour in this.microBehaviours)
+            {
+                microBehaviour.OnDisable();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            foreach (var microBehaviour in this.microBehaviours)
+            {
+                microBehaviour.OnDestroy();
+            }
+        }
+    }
+}
