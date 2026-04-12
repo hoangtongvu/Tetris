48f43fce91a0daa37fa4ab8431eaf3b8482db08f
hoangtongvu <hoangtongvu@gmail.com>
2026-04-12 09:53:54 +0700

    Add AsSpan for List

---
+++ Assets/Scripts/Game.Common/System.Runtime.InteropServices/CollectionsMarshal.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace System.Runtime.InteropServices;
+
+public static class CollectionsMarshal
+{
+    public static Span<T> AsSpan<T>(List<T> list)
+    {
+        if (list == null)
+            return default;
+
+        return Unsafe.As<StrongBox<T[]>>(list).Value;
+    }
+}
