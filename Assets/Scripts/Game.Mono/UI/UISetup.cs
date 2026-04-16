87b6b9366f8f8c2849444a81cf6a3e333320770b
hoangtongvu <hoangtongvu@gmail.com>
2026-04-16 15:10:28 +0700

    Add UISetup

---
+++ Assets/Scripts/Game.Mono/UI/UISetup.cs
@@ -0,0 +1,123 @@
+using Game.Common;
+using Game.UI.Common;
+using Game.UI.Common.MyCanvas;
+using Game.UI.Common.Pooling;
+using Game.UI.Common.UIRendering;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.SceneManagement;
+
+namespace Game.Mono.GameStates
+{
+    public class UISetup : SaiMonoBehaviour
+    {
+        private const string uiPrefabLabel = "UI Prefab";
+
+        private void Awake()
+        {
+            var poolMap = SharedUICtrlPoolMap.Instance;
+            var prefabs = Addressables.LoadAssetsAsync<GameObject>(uiPrefabLabel).WaitForCompletion();
+
+            this.CreateUIToolkitHolders(out var uitkOverlayHolder, out var uitkWorldSpaceHolder);
+
+            foreach (var prefab in prefabs)
+            {
+                var uiCtrl = prefab.GetComponent<BaseUICtrl>();
+                var uiType = uiCtrl.GetUIType();
+                var uiRenderingConfig = uiCtrl.UIRenderingConfig;
+
+                var defaultHolderTransform = this.GetDefaultTransformHolder(
+                    uiRenderingConfig.RenderingType,
+                    uiRenderingConfig.RenderingSpace,
+                    uiType,
+                    uitkOverlayHolder,
+                    uitkWorldSpaceHolder);
+
+                poolMap.Pools.Add(uiType, new()
+                {
+                    Prefab = prefab,
+                    DefaultHolderTransform = defaultHolderTransform,
+                });
+            }
+        }
+
+        private void CreateUIToolkitHolders(out Transform uitkOverlayHolder, out Transform uitkWorldSpaceHolder)
+        {
+            var scene = SceneManager.CreateScene("UIToolkit Scene");
+
+            var overlayHolderGO = new GameObject("OverlayUIsHolder");
+            var worldSpaceHolderGO = new GameObject("WorldSpaceUIsHolder");
+            SceneManager.MoveGameObjectToScene(overlayHolderGO, scene);
+            SceneManager.MoveGameObjectToScene(worldSpaceHolderGO, scene);
+
+            uitkOverlayHolder = overlayHolderGO.transform;
+            uitkWorldSpaceHolder = worldSpaceHolderGO.transform;
+        }
+
+        private Transform GetDefaultTransformHolder(
+            RenderingType renderingType,
+            RenderingSpace renderingSpace,
+            UIType uiType,
+            Transform uitkOverlayHolder,
+            Transform uitkWorldSpaceHolder)
+        {
+            return renderingType switch
+            {
+                RenderingType.UIToolkit => renderingSpace switch
+                {
+                    RenderingSpace.Overlay => this.CreateUIToolkitPool(uiType, uitkOverlayHolder),
+                    RenderingSpace.WorldSpace => this.CreateUIToolkitPool(uiType, uitkWorldSpaceHolder),
+                    _ => null,
+                },
+                //RenderingType.UGUI => this.CreateUGUIPool(
+                //    canvasesCtrl,
+                //    uiType,
+                //    renderingSpace,
+                //    canvasAnchorPreset),
+                //_ => null,
+            };
+        }
+
+        private Transform CreateUIToolkitPool(
+            UIType uiType,
+            Transform baseHolder)
+        {
+            GameObject newGameObject = new($"{uiType}_Pool");
+            newGameObject.transform.SetParent(baseHolder);
+
+            return newGameObject.transform;
+        }
+
+        private Transform CreateUGUIPool(
+            CanvasesCtrl canvasesCtrl,
+            UIType uiType,
+            RenderingSpace renderingSpace,
+            CanvasAnchorPreset canvasAnchorPreset)
+        {
+            GameObject newGameObject = new($"{uiType}_Pool");
+            newGameObject.AddComponent<RectTransform>();
+
+            newGameObject.transform.SetParent(this.GetUGUIParentTransform(canvasesCtrl, renderingSpace, canvasAnchorPreset));
+            newGameObject.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+
+            return newGameObject.transform;
+        }
+
+        private Transform GetUGUIParentTransform(
+            CanvasesCtrl canvasesCtrl,
+            RenderingSpace renderingSpace,
+            CanvasAnchorPreset canvasAnchorPreset)
+        {
+            switch (renderingSpace)
+            {
+                case RenderingSpace.WorldSpace:
+                    return canvasesCtrl.WorldSpaceCanvasManager.transform;
+                case RenderingSpace.Overlay:
+                    int index = (int)canvasAnchorPreset;
+                    return canvasesCtrl.OverlayCanvasManager.GetAnchorPresetTransform(index);
+                default:
+                    return null;
+            }
+        }
+    }
+}
