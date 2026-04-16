9805d86b035261cdd62d2451f6800800a8a247b7
hoangtongvu <hoangtongvu@gmail.com>
2026-04-16 14:57:45 +0700

    Add Execute() to GameCommandPairListsExtensions

--- Assets/Scripts/Game.Domain/Utilities/GameCommands/GameCommandPairListsExtensions.cs
+++ Assets/Scripts/Game.Domain/Utilities/GameCommands/GameCommandPairListsExtensions.cs
@@ -6,6 +6,18 @@ namespace Game.Domain.Utilities.GameCommands;
 
 public static class GameCommandPairListsExtensions
 {
+    public static void Execute(this List<GameCommandPairList> pairLists)
+    {
+        foreach (var pairList in pairLists)
+        {
+            foreach (var pair in pairList.Value)
+            {
+                pair.GameCommand?.Execute();
+                pair.GameCommandAsync?.Execute().AsTask().Wait();
+            }
+        }
+    }
+
     public static async UniTask ExecuteAsyncFrameByFrame(this List<GameCommandPairList> pairLists)
     {
         foreach (var pairList in pairLists)
