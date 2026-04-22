9dc97f2d7a7612799aa57f4e4061331e3981b742
hoangtongvu <hoangtongvu@gmail.com>
2026-04-22 22:25:38 +0700

    Add back button to BootScreen

--- Assets/Scripts/Game.UI/BootScreen/BootScreen_Ctrl.cs
+++ Assets/Scripts/Game.UI/BootScreen/BootScreen_Ctrl.cs
@@ -1,4 +1,5 @@
 using Game.UI.Common;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Game.UI.BootScreen
@@ -8,13 +9,22 @@ namespace Game.UI.BootScreen
     {
         public static BootScreen_Ctrl Instance;
 
+        [SerializeField] private CommandButton backButton;
         private OverlayViewStack overlayViewStack;
+        public BeforeGameplayData BeforeGameplayData = new();
 
         public OverlayViewStack OverlayViewStack => overlayViewStack;
 
         protected override void LoadUIElements()
         {
             this.overlayViewStack = this.uiDocument.rootVisualElement.Q<OverlayViewStack>();
+            this.backButton.Initialize(this.uiDocument);
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            this.backButton.Bind();
         }
 
         public override void OnRent()
