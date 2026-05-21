d172647fc69bf7651084044c527e5dd49b074a78
hoangtongvu <hoangtongvu@gmail.com>
2026-05-21 15:42:52 +0700

    Add Boot, MainMenu, Gameplay scene

---
+++ Assets/Scripts/Game.Mono/GameCommands/Misc/ChangeSceneCommand.cs
@@ -0,0 +1,17 @@
+﻿using Game.Domain.GameCommands;
+using SaintsField;
+using System;
+using UnityEngine.SceneManagement;
+
+namespace Game.Mono.GameCommands;
+
+[Serializable]
+public class ChangeSceneCommand : IGameCommand
+{
+    [Scene] public int SceneInt;
+
+    public void Execute()
+    {
+        SceneManager.LoadScene(this.SceneInt);
+    }
+}
---
+++ Assets/Scripts/Game.UI/GameplayScreen/GameplayScreen_Ctrl.cs
@@ -0,0 +1,17 @@
+using Game.UI.Common;
+
+namespace Game.UI.GameplayScreen
+{
+    public class GameplayScreen_Ctrl : BaseUITKCtrl
+    {
+        public override UIType GetUIType() => UIType.GameplayScreen;
+
+        public override void OnRent()
+        {
+        }
+
+        public override void OnReturn()
+        {
+        }
+    }
+}
